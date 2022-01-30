6afcb5056a3989c9d5db75af1c6b491f76fda555
ante <ante.kotarac@gmail.com>
2022-01-30 20:28:08 +0100

    screen sice to 700 and caption

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from screen import Screen
 class Setup:
     def __init__(self) -> None:
         pygame.init()
+        pygame.display.set_caption('Space invaders')
 
     def get_display(self):
         return pygame.display.set_mode((Screen.WIDTH, Screen.HEIGHT))
