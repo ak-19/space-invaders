2d560573b71a2d03698e1f064625d7853fc4c0bf
ante <ante.kotarac@gmail.com>
2022-01-30 22:09:22 +0100

    payer update

--- player.py
+++ player.py
@@ -6,22 +6,32 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load('assets/spaceship.png')
         self.rect = self.image.get_rect()
-        self.bullet_group = bullet_group
 
         self.rect.centerx = Screen.WIDTH // 2
-        self.rect.y = Screen.HEIGHT - 80
+        self.rect.bottom = Screen.HEIGHT
+
+        self.bullet_group = bullet_group
+
+        self.lives = 5
+        self.velocity = 8
+
+        self.shoot_sound = pygame.mixer.Sound('assets/player_fire.wav')
+        self.shoot_sound.set_volume(.2)
 
     def update(self):
         pressed_keys = pygame.key.get_pressed()
         if pressed_keys[pygame.K_LEFT] and self.rect.x >= 0:
-            self.rect.x -= 10
+            self.rect.x -= self.velocity
         elif pressed_keys[pygame.K_RIGHT] and self.rect.right <= Screen.WIDTH:
-            self.rect.x += 10   
+            self.rect.x += self.velocity
+
+        if pressed_keys[pygame.K_SPACE]:
+            self.shoot()
 
     #helper methods    
     def reset(self):
         pass
 
     def shoot(self):
-        pass    
+        self.shoot_sound.play()
              
